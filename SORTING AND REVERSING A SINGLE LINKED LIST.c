c887edfb4571ac2502ecb34181cbd16f69a71b2f
ROHAN-BHUTANI <bhutanirohan1305@gmail.com>
2024-09-24 09:53:31 +0530

    Update SORTING AND REVERSING A SINGLE LINKED LIST.c

--- SORTING AND REVERSING A SINGLE LINKED LIST.c
+++ SORTING AND REVERSING A SINGLE LINKED LIST.c
@@ -7,7 +7,7 @@ typedef struct linked_list
 	struct linked_list *next;
 }ll;
 void push(ll**,ll**);
-void sort(ll**);
+void sort(ll**,ll**);
 void rev(ll*);
 int main()
 {
@@ -23,7 +23,7 @@ int main()
 			push(&f,&r);
 			break;
 		case 2:
-			sort(&f);
+			sort(&f,&r);
 			break;
 		case 3:
 			rev(f);
@@ -52,7 +52,7 @@ void push(ll**f,ll**r)
 		}
 	}
 }
-void sort(ll**f)
+void sort(ll**f,ll**r)
 {
 	int  i,j,n=0,t;
 	ll *m=*f;
